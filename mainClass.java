d4eb042a40318d3e6a44a2a1e53003f7706257c5
Jake Arnett <jarnett34.js@gmail.com>
2023-11-22 11:38:24 -0500

    attempting to work through self-imposed error

--- mainClass.java
+++ mainClass.java
@@ -1,6 +1,6 @@
 public class mainClass {
     public static void main(String[] args) {
         System.out.println("Hello Class!");
-        System.out.println(4 + 4);
+        System.out.println(6 + 6);
     }
 }
